9af8414ae101a6745173c2a4beff23782e40a2bc
lybb-web <lybb@uci.edu>
2025-04-04 19:50:55 -0700

    Stuffed Home menu into IIFE

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log("Your life is NOTHING!");
+// console.log("Your life is NOTHING!");
+// // let body = document.querySelector("body");
+// let h5 = document.createElement("h5");
+// // h5.textContent = "your life is EVERYTHING!";
+// // body.appendChild(h5);
+
+
+// let test = document.getElementById("test");
+
+// console.log(test.textContent);
+
+
+// document.body.appendChild(h5)
+
+
+
+
+// let content = document.getElementById("content");
+// let h2 = document.createElement("h2");
+// h2.textContent = "Welcome to One of the Restaurants of All Time!!!!!";
+// content.appendChild(h2);
+
+// let h3 = document.createElement("h3");
+// h3.textContent = "10/10 Would eat at again.";
+// content.appendChild(h3);
+
+// let h6 = document.createElement("h6");
+// h6.textContent = "- Gordon Ramsey";
+// content.appendChild(h6);
+
+
+
+const homeMenu = (function() {
+    console.log("we're stuck on a different planet!");
+    let content = document.getElementById("content");
+    let h2 = document.createElement("h2");
+    h2.textContent = "Welcome to One of the Restaurants of All Time!!!!!";
+    content.appendChild(h2);
+
+    let h3 = document.createElement("h3");
+    h3.textContent = "10/10 Would eat at again.";
+    content.appendChild(h3);
+
+    let h6 = document.createElement("h6");
+    h6.textContent = "- Gordon Ramsey";
+    content.appendChild(h6);
+    let menuNav = document.getElementById("menu-button");
+    menuNav.addEventListener("click", (e) =>
+        {
+            console.log("I need a bigger gun!");
+
+        })
+});
+
+
+
+
+
+
+
+homeMenu()
